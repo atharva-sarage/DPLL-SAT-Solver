b6f0597a82f05cbfdeb4116e3e916ffba6e1ba35
atharva-sarage <atharva.sarage@gmail.com>
2020-02-02 00:54:36 +0530

    more optimized

--- solver.cpp
+++ solver.cpp
@@ -7,12 +7,14 @@ void here(){
 class clause{
     public:
         bool sat=false,tautology=false;
+        int totalLiterals=0;
         unordered_set<int>literals;  
         clause(){}
         clause(int unitLiteral){
             literals.insert(unitLiteral);
         }
         inline void addLiteral(int x){
+            totalLiterals++;
             if(x<0){
                 literals.insert(-2*x-1);
                 tautology|=(literals.find(-2*x)!=literals.end());
@@ -32,59 +34,46 @@ class clauseSet{
     private:
         int numClauses,numLiterals;    
         vector <bool> visited;
-
     public:
         vector <clause> clauses;
+        vector <int> countClause;
+        vector <bool> satClause;
         multimap<int,int> literalMap;
         vector <int> unitClauses;
 
         clauseSet(int numliteral,int numclause):numClauses(numclause),numLiterals(numliteral){      
-            clauses.reserve(numClauses+5);
+            countClause.reserve(numClauses+5);
             visited.reserve(2*(numLiterals)+5);
+            countClause.push_back(0);
+            satClause.push_back(false);
             clauses.push_back(clause(0));
+            satClause.reserve(numClauses+5);          
         }
         void addClause(clause cs,bool set){
             if(cs.tautology)
                 return;
             else{
-                clauses.push_back(cs); // 1 based indexing for literal map
+                clauses.push_back(cs);
+                satClause.push_back(false);
+                countClause.push_back(cs.totalLiterals);
                 if(cs.literals.size() == 1){
-                    unitClauses.push_back(clauses.size()-1);
+                    unitClauses.push_back(*(cs.literals.begin()));
                 }
                 if(set){
                     for(auto lit:cs.literals){
-                        literalMap.insert(make_pair(lit,clauses.size()-1));
+                        literalMap.insert(make_pair(lit,countClause.size()-1));
                     }
                 }
             }
         }
-        void removeLastAddedUnitClause(){
-            clause remclause=clauses.back();
-            // int literal=*(remclause.literals.begin());
-            // auto iterpair = literalMap.equal_range(literal);
-            // auto it = iterpair.first;
-            // for (; it != iterpair.second; ++it) {
-            //    if (it->second == literal) { 
-            //         literalMap.erase(it);
-            //         break;
-            //     }
-            // }
-            unitClauses.pop_back();
-            clauses.pop_back();
-        }
         void pureLiteralElim(){
             for(int i=1;i<2*numLiterals;i+=2){
                 if(literalMap.count(i) && literalMap.count(i+1)==0){
-                    addClause(clause(i),false);
+                     unitClauses.push_back(i);
                 }
                 else if(literalMap.count(i+1) && literalMap.count(i)==0){
-                    addClause(clause(i+1),false);
+                    unitClauses.push_back(i+1);
                 }
-            }
-        }
-        void print(){
-            for(auto k:clauses){
-                cout<<k<<endl;
             }
         }
         void printLiteral(int lit){
@@ -94,13 +83,13 @@ class clauseSet{
             cout<<endl;
         }
 };
-int complement(int i){
+inline int complement(int i){
     if(i&1)
         return i+1;
     else
         return i-1;
 }
-int getvariable(int i){
+inline int getvariable(int i){
     if(i&1)
         return (i+1)/2;
     else
@@ -114,55 +103,72 @@ class solver{
         solver(int variables,int clauses):totalClauses(clauses),totalVariables(variables){}
         bool dpll(clauseSet clauseset,unordered_set<int>assigned,unordered_set<int>done,int satisfiedClauses){ 
                 //unit propogation      
-                //clauseset.print();    
+           // cout<<clauseset.unitClauses.size()<<"##"<<endl;
             for(int i=0;i<clauseset.unitClauses.size();i++){
-                auto unitClause=clauseset.unitClauses[i];
-                int unitLiteral=*((clauseset.clauses[unitClause].literals).begin());
-                //cout<<unitLiteral<<"$$$"<<endl;
+                int unitLiteral=clauseset.unitClauses[i];
+                //cout<<"???"<<unitLiteral<<endl;
                 assigned.insert(unitLiteral);
                 done.erase(getvariable(unitLiteral));
+                // done.erase(complement(unitLiteral));
                 auto it = clauseset.literalMap.equal_range(unitLiteral); 
                 for(auto clauseNum=it.first;clauseNum!=it.second;++clauseNum){
-                    if(!clauseset.clauses[clauseNum->second].sat){
-                        clauseset.clauses[clauseNum->second].sat=true;
+                    //cout<<clauseset.satClause[clauseNum->second]<<" **"<<clauseNum->second<<endl;
+                    if(!clauseset.satClause[clauseNum->second]){
+                        clauseset.satClause[clauseNum->second]=true;
+                        // cout<<clauseNum->second<<" true"<<endl;
                         if(clauseNum->second <= totalClauses)
                             satisfiedClauses++;
-                        //cout<<satisfiedClauses<<" "<<totalClauses<<" "<<assigned.size()<<endl;
+                        // for(auto l:assigned)
+                        //     cout<<l<<" ";
+                        // cout<<endl;
+                        // cout<<satisfiedClauses<<" "<<totalClauses<<" "<<assigned.size()<<"??"<<endl;
                         if(satisfiedClauses==totalClauses){
                             finalAssignment=assigned;
                             return true;
                         }
                     }
-                }
+                }              
                 int compUnitLiteral=complement(unitLiteral);
                 //cout<<compUnitLiteral<<endl;
                 it = clauseset.literalMap.equal_range(compUnitLiteral); 
                 for(auto clauseNum=it.first;clauseNum!=it.second;++clauseNum){
-                    clauseset.clauses[clauseNum->second].literals.erase(compUnitLiteral);
-                    if(clauseset.clauses[clauseNum->second].literals.size()==1 && clauseset.clauses[clauseNum->second].sat==false)
-                        clauseset.unitClauses.push_back(clauseNum->second);
-                    else if(clauseset.clauses[clauseNum->second].literals.size()==0){
+                    // clauseset.clauses[clauseNum->second].literals.erase(compUnitLiteral);
+                    clauseset.countClause[clauseNum->second]--;
+                    if(clauseset.countClause[clauseNum->second]==1 && clauseset.satClause[clauseNum->second]==false){
+                        for(auto lit: clauseset.clauses[clauseNum->second].literals){
+                            if(assigned.find(lit)==assigned.end() && assigned.find(complement(lit))==assigned.end())
+                                 clauseset.unitClauses.push_back(lit);
+                        }
+                    }
+                    else if(clauseset.countClause[clauseNum->second]==0){
                         //cout<<"UNSATISFIABLE"<<endl;
                         return false;            
                     }
                 }     
-            }           
+            }       
+            clauseset.unitClauses.clear();    
             //clauseset.print();
             // chose pivot element implement heuristic here
             int pivot = *(done.begin());
+            
+            // pair<int,int> optLit;
+            // for(auto lit:done){
+            //     optLit=max(optLit,{clauseset.literalMap.count(lit),lit});
+            // }
+            // int selectedLiteral=optLit.second;
             int selectedLiteral = 2*pivot;
-            //cout<<pivot<<"??"<<endl;
-            clauseset.addClause(clause(selectedLiteral),false);           
+            //cout<<selectedLiteral<<"??"<<endl;
+            clauseset.unitClauses.push_back(selectedLiteral);
             if(dpll(clauseset,assigned,done,satisfiedClauses))
                 return true;
-            //here();
-            clauseset.removeLastAddedUnitClause();
-            clauseset.addClause(clause(complement(selectedLiteral)),false);
+            clauseset.unitClauses.pop_back();
+            clauseset.unitClauses.push_back(complement(selectedLiteral));
             return dpll(clauseset,assigned,done,satisfiedClauses);            
         }
     // assignment.reserve(2*(numLiterals+5));
 };
 int main(){
+    auto start = chrono::high_resolution_clock::now(); 
     string strOneLine,str;
     int n,m,inp,clauseId=0;
     char ch; 
@@ -174,8 +180,12 @@ int main(){
     cin>>str>>n>>m;
     unordered_set<int>done;
     unordered_set<int>assigned;
+    vector <int> vec;
     for(int i=1;i<=n;i++)
-        done.insert(i);
+        vec.push_back(i);
+    //random_shuffle(vec.begin(),vec.end());
+    for(auto k:vec)
+        done.insert(k);
     clauseSet clauses(n,m);
     vector<int>input;
     while(cin>>inp){
@@ -189,10 +199,16 @@ int main(){
             input.push_back(inp);
         }    
     }
+    // clauses.printLiteral(8);
+    // cout<<"?????????"<<endl;
+    // vector <pair<int,int>> vec;
+    // for(int i=1;i<=2*n;i++)
+    //     vec.push_back({clauses.literalMap.count(i),i});    
+    // sort(vec.begin(),vec.end(),greater<pair<int,int>>());
+    // for(auto k:vec)
+    //     done.insert(k.second);
     clauses.pureLiteralElim();
     solver dpllsolver(n,m);
-    vector<bool>assignment;
-    assignment.reserve(2*n+5);
     cout<<dpllsolver.dpll(clauses,assigned,done,0)<<endl;
     for(auto k:finalAssignment)
         cout<<k<<" ";
@@ -204,6 +220,13 @@ int main(){
     clauses.print();
     clauses.printLiteral(6);
     #endif
-
-    return 0;
+ auto end = chrono::high_resolution_clock::now(); 
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count(); 
+  
+    time_taken *= 1e-9; 
+  
+    cout << "Time taken by program is : " << fixed  
+         << time_taken << setprecision(9); 
+    cout << " sec" << endl; 
+    return 0; 
 }
--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+mkdir dump
 for filename in ./tests/jnh/*.cnf; do
-    echo " $filename" >> out4
+    echo " $filename" >> dump/out5
     #g++ solver.cpp --std=c++11 && ( time ./a.out < "$filename" ) >>out3
-    time ./a2.out < "$filename" >>out4
-    echo "---------------------------" >> out4
+    time ./a2.out < "$filename" >> dump/out5
+    echo "---------------------------" >>  dump/out5
     echo "done, $filename "
 done
