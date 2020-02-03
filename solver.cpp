2d90d67f9543fe94cace8dd98e3e53d57bba28ea
atharva-sarage <atharva.sarage@gmail.com>
2020-02-03 21:47:33 +0530

    5x improvement random selection of literal

--- solver.cpp
+++ solver.cpp
@@ -1,80 +1,74 @@
 #include<bits/stdc++.h>
 //#define DEBUG
+# define IOS ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 using namespace std;
-void here(){
-    cout<<"here"<<endl;
-}
+vector<bool>finalAssignment;
+int totalClauses,totalVariables;
 class clause{
     public:
         bool sat=false,tautology=false;
         int totalLiterals=0;
-        unordered_set<int>literals;  
+        vector<int>literals2;
         clause(){}
         clause(int unitLiteral){
-            literals.insert(unitLiteral);
+            literals2.emplace_back(unitLiteral);
         }
         inline void addLiteral(int x){
             totalLiterals++;
             if(x<0){
-                literals.insert(-2*x-1);
-                tautology|=(literals.find(-2*x)!=literals.end());
+                literals2.emplace_back(-2*x-1);
+                tautology|=(find(literals2.begin(),literals2.end(),2*x)!=literals2.end());
             }
             else{
-                literals.insert(2*x);
-                tautology|=(literals.find(2*x-1)!=literals.end());
+                literals2.emplace_back(2*x);
+                tautology|=(find(literals2.begin(),literals2.end(),-2*x-1)!=literals2.end());
             }
         }
         friend ostream& operator<<(ostream& os, const clause& cs){
-            for(auto lit:cs.literals)
+            for(auto lit:cs.literals2)
                 os<<lit<<" "; 
             return os;
         }
 };
 class clauseSet{
-    private:
-        int numClauses,numLiterals;    
-        vector <bool> visited;
     public:
         vector <clause> clauses;
         vector<vector<int>*> literalMap2; 
         vector <int> countClause;
         vector <bool> satClause;
-        multimap<int,int> literalMap;       
         vector <int> unitClauses;
 
-        clauseSet(int numliteral,int numclause):numClauses(numclause),numLiterals(numliteral){      
-            countClause.reserve(numClauses+5);
-            visited.reserve(2*(numLiterals)+5);
-            literalMap2.reserve(2*(numLiterals)+5);
-            for(int i=0;i<2*numLiterals+5;i++)
+        clauseSet(){      
+            countClause.reserve(totalClauses+5);
+            literalMap2.reserve(2* totalVariables+5);
+            for(int i=0;i<2*totalVariables+5;i++)
                 literalMap2[i]= new vector<int>();
-            countClause.push_back(0);
-            satClause.push_back(false);
-            clauses.push_back(clause(0));
-            satClause.reserve(numClauses+5);          
+            countClause.emplace_back(0);
+            satClause.emplace_back(false);
+            clauses.emplace_back(clause(0));
+            satClause.reserve(totalClauses+5);          
         }
         void addClause(clause cs){
             if(cs.tautology)
                 return;
             else{
-                clauses.push_back(cs);
-                satClause.push_back(false);
-                countClause.push_back(cs.totalLiterals);
-                if(cs.literals.size() == 1)
-                    unitClauses.push_back(*(cs.literals.begin()));
-                for(auto lit:cs.literals){
-                    literalMap.insert(make_pair(lit,countClause.size()-1));             
-                    literalMap2[lit]->push_back(countClause.size()-1);
+                clauses.emplace_back(cs);
+                satClause.emplace_back(false);
+                countClause.emplace_back(cs.totalLiterals);
+                if(cs.literals2.size() == 1)
+                    unitClauses.emplace_back(cs.literals2.front());
+                for(auto lit:cs.literals2){
+                    literalMap2[lit]->emplace_back(countClause.size()-1);
                 }
             }
         }
         void pureLiteralElim(){
-            for(int i=1;i<2*numLiterals;i+=2){
+            for(int i=1;i<2*totalVariables;i+=2){
                 if(literalMap2[i]->size() && literalMap2[i+1]->size()==0){
-                     unitClauses.push_back(i);
+                     unitClauses.emplace_back(i);
                 }
                 else if(literalMap2[i+1]->size() && literalMap2[i]->size()==0){
-                    unitClauses.push_back(i+1);
+                    unitClauses.emplace_back(i+1);
                 }
             }
         }
@@ -96,27 +90,20 @@ inline int getvariable(int i){
     else
         return i/2;
 }
-vector<bool>finalAssignment;
 class solver{
-    private:
-        int totalClauses,totalVariables;
+    private:        
         clauseSet* clauseset;
     public: 
-        solver(int variables,int clauses,clauseSet* cs):totalClauses(clauses),totalVariables(variables),clauseset(cs){}
-        bool dpll(vector<int>unitClauses,vector<int>countClause,vector<bool>satClause,vector<bool>assigned,unordered_set<int>done,int satisfiedClauses){ 
+        solver(clauseSet* cs):clauseset(cs){}
+        bool dpll(vector<int>&unitClauses,vector<int>&countClause,vector<bool>&satClause,vector<bool>&assigned,int satisfiedClauses){ 
                 //unit propogation 
-            bool visited[2*totalVariables+5];
-            memset(visited,false,sizeof(visited));
+            bool visited[2*totalVariables+5]={false};  // memory opt here
             for(int i=0;i<unitClauses.size();i++){
                 int unitLiteral=unitClauses[i];
                 if(visited[unitLiteral])
                     continue;
                 visited[unitLiteral]=true;
                 assigned[unitLiteral]=true;;              
-                done.erase(getvariable(unitLiteral));
-                //clauseset->printLiteral(unitLiteral);
-                //auto it = clauseset->literalMap.equal_range(unitLiteral); 
-               //cout<<clauseset->literalMap2[unitLiteral].size()<<"$$$$"<<endl;
                 if(clauseset->literalMap2[unitLiteral]->size()>0){
                     for(auto clauseNum:*(clauseset->literalMap2[unitLiteral])){
                         //cout<<clauseNum<<endl;
@@ -131,14 +118,14 @@ class solver{
                         }
                     }   
                 }           
-                int compUnitLiteral=complement(unitLiteral);         
-                if(clauseset->literalMap2[compUnitLiteral]->size()>0){
+                int compUnitLiteral=complement(unitLiteral);  
+                if(clauseset->literalMap2[compUnitLiteral]->size()>0){                         
                     for(auto clauseNum:*(clauseset->literalMap2[compUnitLiteral])){
                         countClause[clauseNum]--;
                         if(countClause[clauseNum]==1 && satClause[clauseNum]==false){
-                            for(auto lit: clauseset->clauses[clauseNum].literals){
+                            for(auto lit: clauseset->clauses[clauseNum].literals2){
                                 if(!assigned[lit] && !assigned[complement(lit)]){
-                                    unitClauses.push_back(lit);
+                                    unitClauses.emplace_back(lit);
                                 }
                             }
                         }
@@ -150,38 +137,40 @@ class solver{
             }       
             unitClauses.clear();    
             int temp=0;
-            for(int i=1;i<totalVariables;i++){
-                temp++;
+            int selectedLiteral;
+            for(int i=1;i<=2*totalVariables;i+=2){ // first unassigned
+                if(assigned[i]==false && assigned[i+1]==false)
+                    {selectedLiteral=i;break;}
             }
-            int pivot = *(done.begin());
-            int selectedLiteral = 2*pivot;
-            unitClauses.push_back(selectedLiteral);
-            if(dpll(unitClauses,countClause,satClause,assigned,done,satisfiedClauses))
+            int random=rand()%2;
+            if(random%2==0)
+                selectedLiteral=complement(selectedLiteral);
+            unitClauses.emplace_back(selectedLiteral);
+            vector<int>unitClauses2=unitClauses;
+            vector<int>countClause2=countClause;
+            vector<bool>satClause2=satClause;
+            vector<bool>assigned2=assigned;
+            if(dpll(unitClauses2,countClause2,satClause2,assigned2,satisfiedClauses))
                 return true;
             unitClauses.pop_back();
-            unitClauses.push_back(complement(selectedLiteral));
-            return dpll(unitClauses,countClause,satClause,assigned,done,satisfiedClauses);            
+            unitClauses.emplace_back(complement(selectedLiteral));
+            return dpll(unitClauses,countClause,satClause,assigned,satisfiedClauses);            
         }
 };
 int main(){
+    IOS;
+    srand(time(NULL));
     string strOneLine,str;
-    int n,m,inp,clauseId=0;
+    int inp,clauseId=0;
     char ch; 
     cin>>ch;
     while (ch=='c'){
         getline(cin,strOneLine);
         cin>>ch;
     }
-    cin>>str>>n>>m;
-    unordered_set<int>done;
-    vector<bool>assigned(2*n+5);
-    vector <int> vec;
-    for(int i=1;i<=n;i++)
-        vec.push_back(i);
-    //random_shuffle(vec.begin(),vec.end());
-    for(auto k:vec)
-        done.insert(k);
-    clauseSet clauses(n,m);
+    cin>>str>>totalVariables>>totalClauses;
+    vector<bool>assigned(2*totalVariables+5);
+    clauseSet clauses;
     vector<int>input;
     while(cin>>inp){
         if(inp==0){
@@ -191,28 +180,13 @@ int main(){
             clauses.addClause(cl);
             input.clear();
         }else{
-            input.push_back(inp);
+            input.emplace_back(inp);
         }    
     }
-    //clauses.printLiteral(2);
-    // cout<<"?????????"<<endl;
-    // vector <pair<int,int>> vec;
-    // for(int i=1;i<=2*n;i++)
-    //     vec.push_back({clauses.literalMap.count(i),i});    
-    // sort(vec.begin(),vec.end(),greater<pair<int,int>>());
-    // for(auto k:vec)
-    //     done.insert(k.second);
     clauses.pureLiteralElim();
-    solver dpllsolver(n,m,&clauses);
-    cout<<dpllsolver.dpll(clauses.unitClauses,clauses.countClause,clauses.satClause,assigned,done,0)<<endl;
+    solver dpllsolver(&clauses);
+    cout<<dpllsolver.dpll(clauses.unitClauses,clauses.countClause,clauses.satClause,assigned,0)<<endl;
     for(auto k:finalAssignment)
         cout<<k<<" ";
-    #ifdef DEBUG
-    for(auto k:clauses.unitClauses)
-        cout<<k<<" ";
-    cout<<endl;
-    clauses.print();
-    clauses.printLiteral(6);
-    #endif
     return 0; 
 }
