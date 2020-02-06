057dbe2e65accdf15d83add3e31597cc743334fa
atharva-sarage <atharva.sarage@gmail.com>
2020-02-06 10:49:16 +0530

    state class added but not able to add assigned

--- checker.sh
+++ checker.sh
@@ -4,10 +4,11 @@ mkdir dump
 #CBS_k3_n100_m403_b10
 #UUF200.860.1000
 g++ solver.cpp --std=c++11
-for filename in ./tests/UUF175.753.100/*.cnf; do   
-    #./a.out < $filename &>> ./dump/out27
+for filename in ./tests/CBS_k3_n100_m403_b10/*.cnf; do   
+    #./a.out < $filename &>> ./dump/out30
     time ./a.out < $filename
     # #time ./brute.out < "$filename" >> dump/out6
     echo "$filename"
     # echo "---------------------------" >>  dump/out7    
 done
+#diff -w correctAns.txt ./dump/out30
--- solver.cpp
+++ solver.cpp
@@ -5,75 +5,99 @@ using namespace std;
 vector<bool>finalAssignment;
 int totalClauses,totalVariables;
 class clause{
-    public:
-        bool sat=false,tautology=false;
+    private:
+        bool tautology=false;
         int totalLiterals=0;
-        vector<int>literals2;
+    public:
+        vector<int>literals;
         clause(){}
         clause(int unitLiteral){
-            literals2.emplace_back(unitLiteral);
+            literals.emplace_back(unitLiteral);
         }
         inline void addLiteral(int x){
             totalLiterals++;
             if(x<0){
-                literals2.emplace_back(-2*x-1);
-                tautology|=(find(literals2.begin(),literals2.end(),2*x)!=literals2.end());
+                literals.emplace_back(-2*x-1);
+                tautology|=(find(literals.begin(),literals.end(),2*x)!=literals.end());
             }
             else{
-                literals2.emplace_back(2*x);
-                tautology|=(find(literals2.begin(),literals2.end(),-2*x-1)!=literals2.end());
+                literals.emplace_back(2*x);
+                tautology|=(find(literals.begin(),literals.end(),-2*x-1)!=literals.end());
             }
         }
+        int getCountLiteralsInClause(){
+            return totalLiterals;
+        }
+        bool isTautology(){
+            return tautology;
+        }
         friend ostream& operator<<(ostream& os, const clause& cs){
-            for(auto lit:cs.literals2)
+            for(auto lit:cs.literals)
                 os<<lit<<" "; 
             return os;
         }
 };
+class clauseCurrentState{
+    public:
+        vector <int> countClause;
+        vector <bool> isSatisfied;
+        vector <int> unitClauses;
+    clauseCurrentState(){
+      init();
+    }
+    clauseCurrentState(const clauseCurrentState& state){
+        init();
+        countClause=state.countClause;
+        isSatisfied=state.isSatisfied;
+        unitClauses=state.unitClauses;
+    }
+    void init(){
+        countClause.reserve(totalClauses+5);
+        isSatisfied.reserve(totalClauses+5);          
+        isSatisfied.emplace_back(false);
+        countClause.emplace_back(0);
+    }
+    clauseCurrentState* clone(){
+        return new clauseCurrentState(*this);
+    }
+};
 class clauseSet{
     public:
         vector <clause> clauses;
-        vector<vector<int>*> literalMap2; 
-        vector <int> countClause;
-        vector <bool> satClause;
-        vector <int> unitClauses;
-
-        clauseSet(){      
-            countClause.reserve(totalClauses+5);
-            literalMap2.reserve(2* totalVariables+5);
+        vector<vector<int>*> literalClauseMap;         
+        clauseCurrentState state;
+        clauseSet(){                 
+            literalClauseMap.reserve(2* totalVariables+5);
             for(int i=0;i<2*totalVariables+5;i++)
-                literalMap2[i]= new vector<int>();
-            countClause.emplace_back(0);
-            satClause.emplace_back(false);
-            clauses.emplace_back(clause(0));
-            satClause.reserve(totalClauses+5);          
+                literalClauseMap[i]= new vector<int>();           
+            clauses.emplace_back(clause(0));            
         }
         void addClause(clause cs){
-            if(cs.tautology)
+            if(cs.isTautology())
                 return;
             else{
                 clauses.emplace_back(cs);
-                satClause.emplace_back(false);
-                countClause.emplace_back(cs.totalLiterals);
-                if(cs.literals2.size() == 1)
-                    unitClauses.emplace_back(cs.literals2.front());
-                for(auto lit:cs.literals2){
-                    literalMap2[lit]->emplace_back(countClause.size()-1);
+                state.isSatisfied.emplace_back(false);
+                state.countClause.emplace_back(cs.getCountLiteralsInClause());
+                if(cs.literals.size() == 1)
+                    state.unitClauses.emplace_back(cs.literals.front());
+                for(auto lit:cs.literals){
+                    literalClauseMap[lit]->emplace_back(state.countClause.size()-1);
                 }
             }
         }
         void pureLiteralElim(){
             for(int i=1;i<2*totalVariables;i+=2){
-                if(literalMap2[i]->size() && literalMap2[i+1]->size()==0){
-                     unitClauses.emplace_back(i);
+                if(literalClauseMap[i]->size() && literalClauseMap[i+1]->size()==0){
+                    state.unitClauses.emplace_back(i);
                 }
-                else if(literalMap2[i+1]->size() && literalMap2[i]->size()==0){
-                    unitClauses.emplace_back(i+1);
+                else if(literalClauseMap[i+1]->size() && literalClauseMap[i]->size()==0){
+                    state.unitClauses.emplace_back(i+1);
                 }
             }
         }
         void printLiteral(int lit){
-            for(auto k:(*literalMap2[lit]))
+            for(auto k:(*literalClauseMap[lit]))
                 cout<<k<<" ";
             cout<<endl;
         }
@@ -95,19 +119,20 @@ class solver{
         clauseSet* clauseset;
     public: 
         solver(clauseSet* cs):clauseset(cs){}
-        bool dpll(vector<int>&unitClauses,vector<int>&countClause,vector<bool>&satClause,vector<bool>&assigned,int satisfiedClauses){ 
+        bool dpll(clauseCurrentState& state,vector<bool>&assigned,int satisfiedClauses){ 
                 //unit propogation 
             bool visited[2*totalVariables+5]={false};  // memory opt here
-            for(int i=0;i<unitClauses.size();i++){
-                int unitLiteral=unitClauses[i];
+            for(int i=0;i<state.unitClauses.size();i++){
+                int unitLiteral=state.unitClauses[i];
                 if(visited[unitLiteral])
                     continue;
                 visited[unitLiteral]=true;
-                assigned[unitLiteral]=true;;              
-                if(clauseset->literalMap2[unitLiteral]->size()>0){
-                    for(auto clauseNum:*(clauseset->literalMap2[unitLiteral])){
-                        if(!satClause[clauseNum]){
-                            satClause[clauseNum]=true;
+                assigned[unitLiteral]=true;              
+                if(clauseset->literalClauseMap[unitLiteral]->size()>0){
+
+                    for(auto clauseNum:*(clauseset->literalClauseMap[unitLiteral])){
+                        if(!state.isSatisfied[clauseNum]){
+                            state.isSatisfied[clauseNum]=true;
                             if(clauseNum <= totalClauses)
                                 satisfiedClauses++;
                             if(satisfiedClauses==totalClauses){
@@ -118,63 +143,45 @@ class solver{
                     }   
                 }           
                 int compUnitLiteral=complement(unitLiteral);  
-                if(clauseset->literalMap2[compUnitLiteral]->size()>0){                         
-                    for(auto clauseNum:*(clauseset->literalMap2[compUnitLiteral])){
-                        countClause[clauseNum]--;
-                        if(countClause[clauseNum]==1 && satClause[clauseNum]==false){
-                            for(auto lit: clauseset->clauses[clauseNum].literals2){
+                if(clauseset->literalClauseMap[compUnitLiteral]->size()>0){                         
+                    for(auto clauseNum:*(clauseset->literalClauseMap[compUnitLiteral])){
+                        state.countClause[clauseNum]--;
+                        if(state.countClause[clauseNum]==1 && state.isSatisfied[clauseNum]==false){
+                            for(auto lit: clauseset->clauses[clauseNum].literals){
                                 if(!assigned[lit] && !assigned[complement(lit)]){
-                                    unitClauses.emplace_back(lit);
+                                    state.unitClauses.emplace_back(lit);
                                 }
                             }
                         }
-                        else if(countClause[clauseNum]==0 && satClause[clauseNum]==false){
+                        else if(state.countClause[clauseNum]==0 && state.isSatisfied[clauseNum]==false){
                             return false;                                
                         }
                     }   
                 }  
             }       
-            unitClauses.clear();    
+            state.unitClauses.clear();    
             int temp=0;
             int selectedLiteral;
-
-            // heuristic selection is giving high performance improvements
-            
-            //smallestClauseHeuristic
-            // pair<int,int>selectedClause={1e6,0};
-            // for(int i=1;i<=totalClauses;i++){
-            //     if(satClause[i])
-            //         continue;
-            //     else
-            //         selectedClause=min(selectedClause,{countClause[i],i});                
-            // }
-            // for(auto lit: clauseset->clauses[selectedClause.second].literals2){
-            //     if(!assigned[lit] && !assigned[complement(lit)])
-            //         {selectedLiteral=lit;break;}
-            // }
-
+          
             //DLCS Heuristic
             pair<int,int>selectedVariable={0,0};
             for(int i=1;i<=2*totalVariables;i+=2){
                 if(!assigned[i] && !assigned[i+1]){
-                    selectedVariable=max(selectedVariable,{clauseset->literalMap2[i]->size()+clauseset->literalMap2[i+1]->size(),i});
-                    //selectedVariable=max(selectedVariable,{max(clauseset->literalMap2[i]->size(),clauseset->literalMap2[i+1]->size()),i});                    
+                    selectedVariable=max(selectedVariable,{clauseset->literalClauseMap[i]->size()+clauseset->literalClauseMap[i+1]->size(),i});
                 }
             }
             selectedLiteral=selectedVariable.second;
             int random=rand()%2;
             if(random%2==0)
                 selectedLiteral=complement(selectedLiteral);
-            unitClauses.emplace_back(selectedLiteral);
-            vector<int>unitClauses2=unitClauses;
-            vector<int>countClause2=countClause;
-            vector<bool>satClause2=satClause;
+            state.unitClauses.emplace_back(selectedLiteral);
+            clauseCurrentState prototypeState=clauseCurrentState(state);
             vector<bool>assigned2=assigned;
-            if(dpll(unitClauses2,countClause2,satClause2,assigned2,satisfiedClauses))
+            if(dpll(prototypeState,assigned2,satisfiedClauses))
                 return true;
-            unitClauses.pop_back();
-            unitClauses.emplace_back(complement(selectedLiteral));
-            return dpll(unitClauses,countClause,satClause,assigned,satisfiedClauses);            
+            state.unitClauses.pop_back();
+            state.unitClauses.emplace_back(complement(selectedLiteral));
+            return dpll(state,assigned,satisfiedClauses);            
         }
 };
 int main(){
@@ -189,8 +196,8 @@ int main(){
         cin>>ch;
     }
     cin>>str>>totalVariables>>totalClauses;
-    vector<bool>assigned(2*totalVariables+5);
     clauseSet clauses;
+    vector<bool>assigned(2*totalVariables+5);
     vector<int>input;
     while(cin>>inp){
         if(inp==0){
@@ -205,7 +212,7 @@ int main(){
     }
     clauses.pureLiteralElim();
     solver dpllsolver(&clauses);
-    int ret=dpllsolver.dpll(clauses.unitClauses,clauses.countClause,clauses.satClause,assigned,0);
+    int ret=dpllsolver.dpll(clauses.state,assigned,0);
     cout<<ret<<endl;
     // if(!ret)
     //     cout<<"UNSAT\n";
