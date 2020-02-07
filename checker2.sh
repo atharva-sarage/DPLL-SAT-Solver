0e4cbc9c2d515a1058f7b258be210cee5d10709f
atharva-sarage <atharva.sarage@gmail.com>
2020-02-07 23:40:06 +0530

    done

--- checker2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir dump
-for filename in ./tests/UUF125.538.100/*.cnf; do   
-    ./brute3.out < $filename &>>./dump/out20
-    # #time ./brute.out < "$filename" >> dump/out6
-    # echo "---------------------------" >>  dump/out7    
-    echo "$filename"
-done
